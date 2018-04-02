73abd602987e975bd17e5ce5e3680769c947e063
shieldsy123 <michael.t.shields@gmail.com>
2018-04-02 16:06:21 -0600

    This allegedly works!

--- routes/offer.js
+++ routes/offer.js
@@ -13,22 +13,57 @@ router.get('/', function (req, res, next) {
 
 // GET a single offer
 router.get('/:id', function (req, res, next) {
-  res.send('GET a single offer')
-})
+  // res.send('GET a single offer')
+  const { id } = req.params
+
+  knex('offer')
+    .select('id', 'name')
+    .where('id', id)
+    .then(rows => {
+      if(rows.length > 0) {
+      res.json(rows)
+    }
+    else {
+      res.sendStatus(404)
+    }
+  })
+  })
 
 // POST a new offer
 router.post('/', function (req, res, next) {
-  res.send('POST a new offer')
+  // res.send('POST a new offer')
+  const { name } = req.body
+
+  knex('offer')
+    .insert([
+      { name: name }
+    ])
+    .returning('id')
+    .then(result => res.send(result))
 })
 
 // DELETE an offer
 router.delete('/:id', function (req, res, next) {
-  res.send('DELETE an offer')
+  // res.send('DELETE an offer')
+  const { id } = req.params
+
+  knex('offer')
+    .del()
+    .where('id', id)
+    .then(result => res.json(result))
 })
 
 // UPDATE an offer
 router.patch('/:id', function (req, res, next) {
-  res.send('UPDATE an offer')
+  // res.send('UPDATE an offer')
+  const { id } = req.params
+  const { name } = req.body
+
+  knex('offer')
+    .update({ name: name })
+    .where('id', id)
+    .returning('id')
+    .then(result => res.json(result))
 })
 
 

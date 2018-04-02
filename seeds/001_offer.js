73a6a4b671c2bba68c2fdc52768e2b118f7f7459
shieldsy123 <michael.t.shields@gmail.com>
2018-04-02 15:23:24 -0600

    planted the seeds

---
+++ seeds/001_offer.js
@@ -0,0 +1,15 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('offer').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('offer').insert([
+        {id: 1, name: '25% off your purchase on the first of never.'}
+        {id: 2, name: '25% off your purchase on the first of never.'}
+      ])
+      .then(() => {
+        return knex.raw('SELECT setval("offer_id_seq", (SELECT MAX(id) FROM offer));')
+      })
+    })
+
+}
